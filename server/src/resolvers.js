96ce60dac8cdcf490ac279d6c83cfb92e9d4679e
Maksim Drobchak <maksim.drobchak@kenshoo.com>
2021-02-28 03:20:21 +0200

    update server Mutation for Author and Post

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,14 +5,68 @@ const resolvers = {
     author: (_, args) => Author.findOne({ id: args.id }),
     allAuthors: (_, args) => Author.findAll(),
     allPosts: (_, args) => Post.findAll(),
-    post: (_, args) =>Post.findOne({ id: args.id })
+    post: (_, args) => Post.findOne({ id: args.id })
   },
   Author: {
     posts: (author) => author.getPosts()
   },
+  Response: {
+    success: (response) => response.success,
+    error: (response) => response.error,
+    messsage: (response) => response.messsage,
+  },
   Post: {
-    author: post => post.getAuthor(),
-    views: post => View.findOne({ postId: post.id }).then(view => view.views)
+    author: post => post.getAuthor()
+  },
+  Mutation: {
+    addPost: async (_, { post }) => {
+      try {
+        await Post.create(post);
+        return { success: true, error: "" };
+      } catch (error) {
+        return { success: false, error: "Create post filed!", messsage: error.messsage };
+      }
+    },
+    deletePost: async (_, { id }) => {
+      try {
+        await Post.destroy({ where: { id } });
+        return { success: true, error: "" };
+      } catch (error) {
+        return { success: false, error: "Delete post filed!", messsage: error.messsage };
+      }
+    },
+    updatePost: async (_, { title, text, id }) => {
+      try {
+        await Post.update({ title, text }, { where: { id } });
+        return { success: true, error: "" };
+      } catch (error) {
+        return { success: false, error: "Update post filed!", messsage: error.messsage };
+      }
+    },
+    addAuthor: async (_, { firstName, lastName, posts }) => {
+      try {
+        await Author.create({firstName, lastName, posts: posts ? JSON.parse(posts) : [] });
+        return { success: true, error: "" };
+      } catch (error) {
+        return { success: false, error: "Create autrhor filed!", messsage: error.messsage };
+      }
+    },
+    deleteAuthor: async (_, { id }) => {
+      try {
+        await Author.destroy({ where: { id } });
+        return { success: true, error: "" };
+      } catch (error) {
+        return { success: false, error: "Delete autrhor filed!", messsage: error.messsage };
+      }
+    },
+    updateAuthor: async (_, { firstName, lastName, posts, id }) => {
+      try {
+        await Author.update({ firstName, lastName, posts: posts ? JSON.parse(posts) : [] }, { where: { id } });
+        return { success: true, error: "" };
+      } catch (error) {
+        return { success: false, error: "Update autrhor filed!", messsage: error.messsage };
+      }
+    }
   }
 };
 
--- server/src/schema.js
+++ server/src/schema.js
@@ -2,25 +2,67 @@ const { makeExecutableSchema } = require("graphql-tools");
 const resolvers = require("./resolvers");
 
 const typeDefs = `
-type Query {
-  author(id: ID!): Author
-  post(id: ID!): Post
-  allAuthors: [Author]
-  allPosts: [Post]
-}
-type Author {
-  id: ID!
-  firstName: String
-  lastName: String
-  posts: [Post]
-}
-type Post {
-  id: ID!
-  title: String
-  text: String
-  views: Int
-  author: Author
-}
+  type Query {
+    author(id: ID!): Author
+    post(id: ID!): Post
+    allAuthors: [Author]
+    allPosts: [Post]
+  }
+
+  type Author {
+    id: ID!
+    firstName: String
+    lastName: String
+    posts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    title: String
+    text: String
+    views: Int
+    author: Author
+  }
+
+  type Response {
+    success: Boolean!
+    error: String!
+    messsage: String
+  }
+
+  type Mutation {
+    addPost(
+      id: ID!
+      title: String
+      text: String
+      views: Int
+      ): Response
+
+    deletePost(id: ID): Response
+
+    updatePost(
+      id: ID!
+      title: String
+      text: String
+      views: Int
+    ): Response
+
+    addAuthor(
+      id: ID!
+      firstName: String!
+      lastName: String!
+      posts: String
+    ): Response
+
+    deleteAuthor(id: ID): Response
+
+    updateAuthor(
+      id: ID!
+      firstName: String
+      lastName: String
+      posts: String
+    ): Response
+  }
 `;
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
